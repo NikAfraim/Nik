a8df6f3875dc1207c9e8c300bdb97d7de050d3d6
Никита Сенгилейцев <NikSen09@mail.ru>
2022-03-15 23:00:54 +0400

    create class vehicle

--- string.py
+++ string.py
@@ -1,5 +1,22 @@
-def upper(test: str) -> str:
-    test = test.upper()
-    return test
+def upper(text: str) -> str:
+    text = text.upper()
+    return text
+
 
 test = upper('hello')
+print(test)
+
+
+class Vehicle:
+    fuel: int = 'sds'
+
+    def printout(self):
+        print(self.fuel)
+
+
+car = Vehicle()
+car1 = Vehicle()
+Vehicle.fuel = 'hx'
+car.fuel = 300
+Vehicle.printout(car)
+Vehicle.printout(car1)
